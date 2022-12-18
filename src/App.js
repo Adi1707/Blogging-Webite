ceb89a3652d43d284a3378c28d73310026551dea
ADITYA PANDEY <adityap1707@gmail.com>
2022-12-18 09:39:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Home from './Home'
 function App() {
   return (
     <div className="App">
-
       <Navbar />
-
       <div className="content">
         <Home />
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    // let name = 'Aditya';
+    const [name, setname] = useState(['aditya']);  //reactive value
+    const [age, setage] = useState('19');
+    const handleClick = () => {
+        setname('Aditya Pandey');
+        setage('20');
+    } 
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} old.</p>
+            <button className="btn" onClick={handleClick}>Click Me</button>
+            
         </div>
      );
 }
